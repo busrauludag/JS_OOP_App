4e3635582b938b1336cf314bdf0446c66acfae09
busrauludag <busrauludag13@gmail.com>
2017-12-24 22:04:49 +0300

    continue

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log("this is app.js");
+class Drone{
+
+  constructor(id, name){
+    this.id = id;
+    this.name = name;
+
+    console.log(this.id, this.name);
+  }
+
+}
+
+let drone = new Drone(123, 'Büşra');
+console.log(drone.id, drone.name);
+
+// console.log(typeof drone);
+// console.log(drone instanceof Drone);
